21c2969ccea573b5f7f00f459a625b757c48dc75
kamrul <kamrul444216@gmail.com>
2024-06-26 23:45:11 +0600

    aggrigate product and show with review

--- src/app/modules/product/product.services.ts
+++ src/app/modules/product/product.services.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { TProduct } from "./product.interface";
 import { Product } from "./product.model";
 
@@ -7,14 +8,166 @@ const createProduct = async (product: TProduct) => {
 };
 
 const getAllProducts = async () => {
-  const products = await Product.find().populate("category");
+  const session = await mongoose.startSession();
+  let products;
+
+  try {
+    session.startTransaction();
+
+    products = await Product.aggregate([
+      {
+        $lookup: {
+          from: 'reviews',
+          localField: '_id',
+          foreignField: 'product',
+          as: 'reviews'
+        }
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'reviews.user',
+          foreignField: '_id',
+          as: 'userDetails'
+        }
+      },
+      {
+        $addFields: {
+          reviews: {
+            $map: {
+              input: "$reviews",
+              as: "review",
+              in: {
+                $mergeObjects: [
+                  "$$review",
+                  {
+                    userDetails: {
+                      $arrayElemAt: [
+                        {
+                          $filter: {
+                            input: "$userDetails",
+                            as: "user",
+                            cond: { $eq: ["$$user._id", "$$review.user"] }
+                          }
+                        },
+                        0
+                      ]
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: 'categories',
+          localField: 'category',
+          foreignField: '_id',
+          as: 'categoryDetails'
+        }
+      },
+      {
+        $unwind: {
+          path: '$categoryDetails',
+          preserveNullAndEmptyArrays: true
+        }
+      },
+      {
+        $project: {
+          userDetails: 0
+        }
+      }
+    ]);
+
+    await session.commitTransaction();
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    session.endSession();
+  }
+
   return products;
 };
 
+
+
 const getSingleProduct = async (id: string) => {
-  const product = await Product.findById(id).populate("category");
-  return product;
+  const product = await Product.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(id) }
+    },
+    {
+      $lookup: {
+        from: 'reviews',
+        localField: '_id',
+        foreignField: 'product',
+        as: 'reviews'
+      }
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'reviews.user',
+        foreignField: '_id',
+        as: 'userDetails'
+      }
+    },
+    {
+      $addFields: {
+        reviews: {
+          $map: {
+            input: "$reviews",
+            as: "review",
+            in: {
+              $mergeObjects: [
+                "$$review",
+                {
+                  userDetails: {
+                    $arrayElemAt: [
+                      {
+                        $filter: {
+                          input: "$userDetails",
+                          as: "user",
+                          cond: { $eq: ["$$user._id", "$$review.user"] }
+                        }
+                      },
+                      0
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    },
+    {
+      $lookup: {
+        from: 'categories',
+        localField: 'category',
+        foreignField: '_id',
+        as: 'categoryDetails'
+      }
+    },
+    {
+      $unwind: {
+        path: '$categoryDetails',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $project: {
+        userDetails: 0
+      }
+    }
+  ]);
+
+  return product.length ? product[0] : null;
 };
+
 
 const editProduct = async (id: string, product: TProduct) => {
   const updatedProduct = await Product.findByIdAndUpdate(id, product, {
